ee5cd93a28094ee6092376182612de9f234ba026
Tristan Webb <tristan@fpcomplete.com>
2015-10-19 20:56:40 -0700

    removing int tests

--- test/integration/tests/115-config-cli/Main.hs
+++
@@ -1,8 +0,0 @@
-import StackTest
-
-main :: IO ()
-main = do
-    -- Test we are at baseline
-    fileContentsMatch ["stack.yaml", "stack.yaml.base"]
-    stack ["config", "set", "resolver", "nightly"]
-    fileContentsMatch ["stack.yaml", "stack.yaml.nightly"]
